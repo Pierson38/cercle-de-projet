1ec49880e02316569cc9f6b4496323ca34352e89
Pierson38 <107060286+Pierson38@users.noreply.github.com>
2022-11-14 13:38:44 +0100

    Initialisation du projet

---
+++ src/pages/NoteApp.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class NoteApp extends Component {
+
+    render() {
+        return (
+            <p>Ce ci e ejofi hfo </p>
+        );
+    }
+}
+
+export default NoteApp;
