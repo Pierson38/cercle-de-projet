25239389c4e5553dfc30fc0bdb5422db1c4c4e58
Louis-source <77414060+Louis-source@users.noreply.github.com>
2022-11-15 15:09:14 +0100

    modif note et message

--- src/data/messages.js
+++ src/data/messages.js
@@ -2,12 +2,14 @@ var messages = [
     {
         id: 0,
         from: "Boss",
-        messages: ["Salut, ca va ?", "Oui et toi ?", "Ca va merci"],
+        messages: [{from: "moi", message: "Salut"}, {from: "Boss", message: "Salut"}, {from: "moi", message: "Ca va ?"}],
+        profilUrl: "http://localhost:3000/assets/contact/icon-bea.png",
     },
     {
         id: 1,
         from: "Bea",
-        messages: ["Tu rentres ce soir ?", "Oui et toi ?", "Cool"],
+        messages: [{from: "moi", message: "Tu rentres ce soir ?"}, {from: "Boss", message: "Oui et toi ?"}, {from: "moi", message: "Cool va ?"}],
+        profilUrl: "http://localhost:3000/assets/contact/icon-bea.png",
     },
 ];
 
--- src/pages/messages/Message.jsx
+++ src/pages/messages/Message.jsx
@@ -8,16 +8,32 @@ function Note({ message }) {
 
   return (
     <div>
-      <div className="p-3">
+      <div className="relative w-screen">
         <Link to=".." relative="path">
-          <p className="cancel">&lt; Cancel</p>
+          <div class="flex p-3 justify-between items-center bg-grey-100">
+            <p className="cancel" class="text-5xl blue -mt-2 ml-2">&#8249;</p>
+            <div class="">
+              <img src={messages[id].profilUrl}/>
+              <h1 className="text-1xl">{messages[id].from}</h1>
+            </div>
+            <div></div>
+          </div>
         </Link>
       </div>
-      <div className="px-6 pb-6 h-screen">
-        <h1 className="font-bold text-3xl">{messages[id].from}</h1>
-        {messages[id].messages.map((item) => (
-          <p className="leading-7 mt-4">{item}</p>
-        ))}
+      <div className="px-6 pb-6 h-screen mt-8">
+        {messages[id].messages.map(item => {
+          return item.from == "moi" ?
+            <div class="flex justify-end"><div class="flex justify-end items-center rounded-2xl px-4 py-1 bg-blue w-fit text-white"> <span className="">{item.message}</span></div></div>
+            :
+            <div class="flex justify-start">
+              <div>
+                <span class="text-sm ml-2 text-grey">{item.from}</span>
+                <div class="flex justify-start items-center rounded-2xl px-4 py-1 bg-grey w-fit"> 
+                  <span className="">{item.message}</span>
+                </div>
+              </div>
+            </div>
+          })}
       </div>
     </div>
   );
