d66b3ff1400dd41e2ff48a80ee3ea0bcfec1a53d
Nicolas <nicolasbardot.g4@gmail.com>
2022-11-15 08:57:29 +0100

    Configuration tailwindcss
    Initialisation des couleurs et de la shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,37 @@ module.exports = {
   ],
   theme: {
     colors: {
+      'orange': {
+        100: '#FFA132',
+        200: '#FF8A00',
+      },
+      'grey': {
+        100: '#F4F7FA',
+        200: '#767680',
+        500: '#868686',
+      },
+      'blue': {
+        100: '#C0F4FF',
+        200: '#007AFF',
+      },
+      'green': {
+        100: '#C0FFD9',
+        200: '#34C759',
+      },
+      'red': {
+        100: '#EFC0FF',
+        200: '#FF3B30',
+      },
     },
     fontFamily: {
       'sans': ['SF_Pro', ...defaultTheme.fontFamily.sans],
     },
-    extend: {},
+    extend: {
+      boxShadow: {
+        'shadow': '0px 4px 11px -4px rgba(0, 0, 0, 0.17)',
+      }
+    },
   },
   plugins: [],
 }
+
