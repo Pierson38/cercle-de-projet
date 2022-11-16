be5819830c84bfbd6e0401cf81bb8f6dd3ce4645
Pierson38 <107060286+Pierson38@users.noreply.github.com>
2022-11-16 15:59:30 +0100

    Notification config

---
+++ src/data/notification.js
@@ -0,0 +1,10 @@
+var notifications = [[
+    {
+        id: 0,
+        title: "Note",
+        content: "Ceci est le content de l'app",
+        icon: "/assets/appIcons/icon-note.png",
+    },
+]];
+
+export default notifications;
