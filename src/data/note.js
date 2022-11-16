73f806a60b98242a2506626d0d5cfb4279e852e4
Viggsengetass <paulito.antoine@gmail.com>
2022-11-16 09:31:11 +0100

    Ajout notes

--- src/data/note.js
+++ src/data/note.js
@@ -3,14 +3,95 @@ var notes = [
         id: 0,
         title: "Travail à faire",
         description:
-            "Quibus ita sceleste patratis Paulus cruore perfusus reversusque ad principis castra multos coopertos paene catenis adduxit in squalorem deiectos atque maestitiam, quorum adventu intendebantur eculei uncosque parabat carnifex et tormenta. et ex is proscripti sunt plures actique in exilium alii, non nullos gladii consumpsere poenales. nec enim quisquam facile meminit sub Constantio, ubi susurro tenus haec movebantur, quemquam absolutum.",
+            "Faire un vrai boulot à Bea.",
     },
     {
         id: 1,
         title: "Demain",
         description:
-            "Quibus ita sceleste patratis Paulus cruore perfusus reversusque ad principis castra multos coopertos paene catenis adduxit in squalorem deiectos atque maestitiam, quorum adventu intendebantur eculei uncosque parabat carnifex et tormenta. et ex is proscripti sunt plures actique in exilium alii, non nullos gladii consumpsere poenales. nec enim quisquam facile meminit sub Constantio, ubi susurro tenus haec movebantur, quemquam absolutum.",
+            "Aller chercher Marina à la gare, acheter du sel pour G4.",
     },
+    {
+        id: 2,
+        title: "Ortho",
+        description:
+            "RDV orthodontiste 26 janvier 15H , Apporter moyen de paiement.",
+    },
+    {
+        id: 3,
+        title: "Gérard",
+        description:
+            "Manager conseil en conduite du changement 06 10 26 37 63",
+    },
+    {
+        id: 4,
+        title: "Entretien Siffredy academy",
+        description:
+            "19 Mars 2022 , venir avec un gros , gros dossier et de gros élements",
+    },
+    {
+        id: 5,
+        title: "Chasse Jaquie",
+        description:
+            "3 decembre chasse au sanglier , prevoir de ramener beaucoup de gland (Faire un tri à G4)",
+    },
+    {
+        id: 6,
+        title: "Liste de course",
+        description:
+            "Carotte, lait, eau , pâte , riz , courgettes, viagra, activia , nouilles , sauce soja , coca, émincée de poulet, steacks hachées, curry , fajitas , chips, lub, bar, limande, saumon, sauje , topinambour.",
+    },
+    {
+        id: 7,
+        title: "A faire à G4 ",
+        description:
+            "Dire à Paul qu'il a encore tout cassé, trouvé la pancarte du 19, racheter du soppalin pour G4.",
+    },
+    {
+        id: 8,
+        title: "Site montage meuble",
+        description:
+            "https://www.frizbiz.com/fr/articles/le-montage-de-meubles-en-toute-simplicite-grace-a-frizbiz-frizbiz , https://www.montermonmeubletoutseul.com/ , https://www.ikea.com/fr/fr/customer-service/knowledge/articles/9950b769-82g9-407g-9bb0-4049bdgb3790.html , https://www.shutterstock.com/fr/search/busty-redhead?image_type=photo , https://www.systemed.fr/mobilier/montage-meubles-kit ,3185.html , https://fr.spankbang.com/s/redhead/ , https://www.but.fr/Services/Page/Montage ",
+    },
+    {
+        id: 9,
+        title: "Rdv Kiné",
+        description:
+            "3 décembre 18h15",
+    },
+
+    {
+        id: 10,
+        title: "liste anniversaire 27/08",
+        description:
+            "Bea, Leopold, Nathalie, Marina, le mec qui remplie le distributeur, Paul , Louis , Evan , Alice, Mathis, Julien , Nico , Mylene",
+    },
+    {
+        id: 11,
+        title: "Afterwork",
+        description:
+            "Afterwork Atos 13 décembre",
+    },
+    {
+        id: 12,
+        title: "Crypto",
+        description:
+            "Binance, Stepn , Minerstate",
+    },
+
+    {
+        id: 13,
+        title: "Framework",
+        description:
+            "Bootstrap, Tailwind",
+    },
+    {
+        id: 14,
+        title: "Rappeler",
+        description:
+            "Rappeler Julien loppizo de la part de Islid",
+    },
+
 ];
 
 export default notes;
