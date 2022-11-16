165a2f8949cb5c0c02560cfa809eaef72dd15759
Louis-source <77414060+Louis-source@users.noreply.github.com>
2022-11-16 15:53:16 +0100

    mise en page message

--- src/pages/messages/Message.jsx
+++ src/pages/messages/Message.jsx
@@ -16,17 +16,17 @@ function Note({ message }) {
               <img src={messages[id].profilUrl} alt="" className="image-ronde w-12 m-auto"/>
               <h1 className="text-1xl text-center">{messages[id].from}</h1>
             </div>
-            <div></div>
+            <div class="w-6"></div>
           </div>
         </Link>
       </div>
       <div className="px-6 pb-6 h-screen mt-8">
         {messages[id].messages.map(item => {
           return item.from === "moi" ?
-            <div class="flex justify-end"><div class="flex justify-end items-center rounded-2xl px-4 py-1 bg-blue w-fit text-white"> <span className="">{item.message}</span></div></div>
+            <div class="flex justify-end mt-4 "><div class="flex justify-end items-center rounded-2xl px-4 py-1 bg-blue w-fit text-white max-width"> <span className="">{item.message}</span></div></div>
             :
-            <div class="flex justify-start">
-              <div>
+            <div class="flex justify-start mt-4 ">
+              <div class="max-width">
                 <span class="text-sm ml-2 text-grey">{item.from}</span>
                 <div class="flex justify-start items-center rounded-2xl px-4 py-1 bg-grey w-fit"> 
                   <span className="">{item.message}</span>
