8c864764d68ec92f00eee71707b8226166be369d
Viggsengetass <paulito.antoine@gmail.com>
2022-11-17 11:53:08 +0100

    Icon notif

--- src/data/notification.js
+++ src/data/notification.js
@@ -1,9 +1,51 @@
 var notifications = [[
     {
         id: 0,
-        title: "Note",
-        content: "Ceci est le content de l'app",
-        icon: "/assets/appIcons/icon-note.png",
+        title: "LinkedIn",
+        content: "Beatriz Quintans souhaite rejoindre votre réseau",
+        icon: "/assets/appIcons/linkedin.png",
+    },
+    {
+        id: 1,
+        title: "Instagram",
+        content: "David a liké votre publication",
+        icon: "/assets/Icon notif/instagram.png",
+    },
+    {
+        id: 2,
+        title: "Mich",
+        content: "Salut, comment tu vas vieille branche?",
+        icon: "/assets/Icon notif/messages.png",
+    },
+    {
+        id: 3,
+        title: "Twitter",
+        content: "Tibo InShape : Combien de café vous buvez par jour ?",
+        icon: "/assets/Icon notif/twitter.png",
+    },
+    {
+        id: 4,
+        title: "Twitter",
+        content: "Elon Musk : Punting relaunch of Blue Verified to November 29th to make sure that it rock solid",
+        icon: "/assets/Icon notif/Twitter.png",
+    },
+    {
+        id: 5,
+        title: "Réglages",
+        content: "Mise à jour système 1.10.5 redémarrage nécessaire",
+        icon: "/assets/Icon notif/settings.png",
+    },
+    {
+        id: 6,
+        title: "Leopold",
+        content: "Salut ça va?",
+        icon: "/assets/Icon notif/messages.png",
+    },
+    {
+        id: 7,
+        title: "Bea",
+        content: "Besoin d'aide pour la recherche d'alternance ?",
+        icon: "/assets/Icon notif/messages.png",
     },
 ]];
 
