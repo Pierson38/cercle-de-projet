8853538e7e36930b8835d62e5ff86ba372e86148
Nicolas <nicolasbardot.g4@gmail.com>
2022-11-17 14:29:30 +0100

    Popup v3

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -6,10 +6,10 @@ function Popup({ popup }) {
   return (
     <div className="h-screen w-full flex justify-center items-center">
 
-      <div className="bg-white bg-shadow rounded-2xl">
+      <div className="bg-white bg-shadow rounded-2xl w-80">
         <div className="flex flex-col items-center justify-center py-8 px-12">
-          <h1 className="font-bold text-xl mb-2">Hola Hooman !</h1>
-          <p className="p-1">This is a popup for homman</p>
+          <h1 className="text-center font-bold text-xl mb-2">Hola Hooman !</h1>
+          <p className="text-center p-1">This is a popup for homman</p>
         </div>
 
         <div className="w-full text-center border-t-grey p-3">
