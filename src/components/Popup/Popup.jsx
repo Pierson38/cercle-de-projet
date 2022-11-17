030cc72e6a19e839a3b37b96911fdc0f2a78273d
Nicolas <nicolasbardot.g4@gmail.com>
2022-11-17 14:16:42 +0100

    Popup

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -4,8 +4,19 @@ import styled, { keyframes } from "styled-components";
 
 function Popup({ popup }) {
   return (
-    <div className="fixed w-full mt-4 notification p-2 flex flex-col mx-4 rounded-xl ">
-        
+    <div className="h-screen w-full flex justify-center items-center">
+
+      <div className="bg-white bg-shadow rounded-2xl">
+        <div className="flex flex-col items-center justify-center py-8 px-12">
+          <h1 className="font-bold text-xl mb-2">Hola Hooman !</h1>
+          <p className="p-1">This is a popup for homman</p>
+        </div>
+
+        <div className="w-full text-center border-t-grey p-3">
+          <button className="text-blue">Ok</button>
+        </div>
+      </div>
+
     </div>
   );
 }
